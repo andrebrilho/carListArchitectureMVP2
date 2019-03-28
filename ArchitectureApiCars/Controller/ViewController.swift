594fb9bda669459b7e1d895c1ca8fc84c7959b72
andrebrilho <andre.txt@hotmail.com>
2019-03-27 22:16:07 -0300

    “primeiro”

--- ArchitectureApiCars/Controller/ViewController.swift
+++ ArchitectureApiCars/Controller/ViewController.swift
@@ -8,26 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController, CarsProtocol {
+protocol updatingViewFromApiProtocol {
+    var listCars:[Carro] {get set}
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, updatingViewFromApiProtocol {
+    
+    var carrosProtocol:CarsProtocol = CarrosPresenter()
+    var listCars: [Carro] = []{
+        didSet{
+            tbl.reloadData()
+        }
+    }
     
     @IBOutlet weak var tbl: UITableView!
-    var tableViewCarDatasourceAndDelegate:TableCarDatasourceAndDelegate?
-    var carrosPresenter:CarrosPresenter?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableViewCarDatasourceAndDelegate = TableCarDatasourceAndDelegate(infoTbl: tbl, viewControllerToPress: self)
-        carrosPresenter = CarrosPresenter.init(protocolCar: self)
+        tbl.delegate = self
+        tbl.dataSource = self
+        tbl.register(UINib(nibName: "CarTableViewCell", bundle: nil), forCellReuseIdentifier: "CarTableViewCell")
+        carrosProtocol.viewControllerReference = self
+        carrosProtocol.getCars()
         
     }
     
-    func presentTableData(carList: [Carro]) {
-        self.tableViewCarDatasourceAndDelegate?.dados = carList
-        self.tbl.reloadData()
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listCars.count
     }
-    
-    func presentAlertError(mensagem: String, titleMessage: String) {
-        Alert.showAlertError(mensagemErro: mensagem, titleMsgErro: titleMessage, view: self)
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tbl.dequeueReusableCell(withIdentifier: "CarTableViewCell", for: indexPath) as? CarTableViewCell{
+            cell.carro = listCars[indexPath.row]
+            return cell
+        }
+        return UITableViewCell()
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 190
     }
 }
 
